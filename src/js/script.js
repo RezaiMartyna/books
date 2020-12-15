2ddafd9b945dc9c739a1b9bba77dc2867b607fcc
Martyna Rezai <martyna.rezai@gmail.com>
2020-12-15 21:58:36 +0100

    new try to fix

--- src/js/script.js
+++ src/js/script.js
@@ -1,27 +1,19 @@
 {
+  const templateBook = Handlebars.compile(document.querySelector('#template-book').innerHTML),
+  console.log('temple', templateBook)
+  const listBookHtml = document.querySelector('.books-list');
+  console.log('list', listBookHtml)
 
-templateBook = Handlebars.compile(document.querySelector('#template-book').innerHTML),
-console.log('temple', templateBook)
-listBookHtml = document.querySelector('.books-list');
-console.log('list', listBookHtml)
-
-
-
-
-function render() {
-  for (const element in dataSource.books) {
-    /*generate HTML based on template */
-    const generatedHTML = templateBook(element);
-    /*create element using utils.creteElementFromHTML */
-    elementhtml = utils.createDOMFromHTML(generatedHTML);
-    /*find menu container*/
-    const menuContainer = listBookHtml
-    /*add element to menu*/
-    menuContainer.appendChild(elementhtml);
+  function render() {
+    for (const element in dataSource.books) {
+      /*generate HTML based on template */
+      const generatedHTML = element(templateBook);
+      /*create element using utils.creteElementFromHTML */
+      const elementhtml = utils.createDOMFromHTML(generatedHTML);
+      /*find menu container*/
+      const menuContainer = listBookHtml
+      /*add element to menu*/
+      menuContainer.appendChild(elementhtml);
+    }
   }
 }
-function render();
-
-}
-
-
