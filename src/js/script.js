71180c964810674261ca71b0f6ffbcaf1fe60079
Martyna Rezai <martyna.rezai@gmail.com>
2020-12-15 22:06:00 +0100

    try to fix3

--- src/js/script.js
+++ src/js/script.js
@@ -5,13 +5,13 @@ const listBookHtml = document.querySelector('.books-list');
 function render() {
   for (const element in dataSource.books) {
     /*generate HTML based on template */
-    const generatedHTML = element(templateBook);
+    const generatedHTML = templateBook(element);
     /*create element using utils.creteElementFromHTML */
     const elementhtml = utils.createDOMFromHTML(generatedHTML);
     /*find menu container*/
-    const menuContainer = listBookHtml
+    const menuContainer = listBookHtml;
     /*add element to menu*/
     menuContainer.appendChild(elementhtml);
-  };
-};
-
+  }
+}
+render();
