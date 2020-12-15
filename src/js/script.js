2f89565616be7348c7176581c8281129f9e819eb
Martyna Rezai <martyna.rezai@gmail.com>
2020-12-15 18:46:03 +0100

    init commit

---
+++ src/js/script.js
@@ -0,0 +1,27 @@
+
+
+templateBook = Handlebars.compile(document.querySelector('#template-book').innerHTML),
+  console.log('temple', templateBook)
+listBookHtml = document.querySelector('.books-list');
+console.log('list', listBookHtml)
+
+
+
+
+function render() {
+  for (const element in dataSource.books) {
+    /*generate HTML based on template */
+    const generatedHTML = templateBook(data);
+    /*create element using utils.creteElementFromHTML */
+    elementhtml = utils.createDOMFromHTML(generatedHTML);
+    /*find menu container*/
+    const menuContainer = listBookHtml
+    /*add element to menu*/
+    menuContainer.appendChild(elementhtml);
+  }
+}
+
+
+
+
+
