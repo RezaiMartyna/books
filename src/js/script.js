5500c4ada25619c5e243215e3449244cd9f1d190
Martyna Rezai <martyna.rezai@gmail.com>
2020-12-17 16:24:38 +0100

    problem with 34

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,47 @@
+//szablon znajdujący się w html
 const templateBook = Handlebars.compile(document.querySelector('#template-book').innerHTML);
+//lista książek w html
 const listBookHtml = document.querySelector('.books-list');
-const elements = dataSource.books
 
 function render() {
-  for (const element of elements) {
-    /*generate HTML based on template */
+  //dla każdego elementu zapisanego w pliku dataSource.books
+  for (const element of dataSource.books) {
+    /*wstaw element do szablonu */
     const generatedHTML = templateBook(element);
-    /*create element using utils.creteElementFromHTML */
+    /*stwórz gotowy html o to co powstało wyrzej */
     const elementhtml = utils.createDOMFromHTML(generatedHTML);
-    /*find book container*/
+    /*znajdz miejsce do którego chcesz włożyc gotowy html*/
     const menuContainer = listBookHtml;
-    /*add element */
+    /*dodaj html do miejsca w którym chcesz zeby sie znajdował */
     menuContainer.appendChild(elementhtml);
   }
-}
+};
+//wywołaj funkcje
 render();
+
+const favoriteBooks = [];
+
+function initActions() {
+
+  ListOfElements = listBookHtml.querySelectorAll('.book__image');
+
+  for (const picture of ListOfElements) {
+    picture.addEventListener('dblclick', function(event){
+      event.preventDefault();
+
+      if (picture.hasAttribute('favorite')){
+        const bookId = picture.getAttribute('data-id');
+        // usunać element z tablicy
+        delete favoriteBooks(bookId);
+        picture.classList.remove('favorite');
+      } else {
+        picture.classList.add('favorite');
+        const bookId = picture.getAttribute('data-id');
+        favoriteBooks.push(bookId);
+        console.log(favoriteBooks, 'ulubione ksiazki')
+      }
+    })
+  }
+};
+initActions();
+
